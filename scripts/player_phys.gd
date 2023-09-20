e8d91576b21055886cb4ab9732dedada23c9f3be
mld <lolxdfly@gmail.com>
2023-09-20 14:23:38 +0200

    updated physics

--- scripts/player_phys.gd
+++ scripts/player_phys.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const DAMPING = 30.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -12,15 +13,15 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, DAMPING)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DAMPING)
 
 	move_and_slide()
