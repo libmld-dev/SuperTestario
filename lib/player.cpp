14ee8e1baca1cdaaa438a63c71032de0157c6877
mld <lolxdfly@gmail.com>
2023-09-21 20:49:46 +0200

    main body animations

--- lib/player.cpp
+++ lib/player.cpp
@@ -3,23 +3,53 @@
 #include <godot_cpp/classes/input.hpp>
 #include <godot_cpp/classes/project_settings.hpp>
 #include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/classes/animated_sprite2d.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
-void Player::_bind_methods() {
+void Player::_bind_methods()
+{
 }
 
-Player::Player() {
+Player::Player() : lookLeft(true)
+{
     if(Engine::get_singleton()->is_editor_hint())
         set_process_mode(Node::ProcessMode::PROCESS_MODE_DISABLED);
+
     gravity = ProjectSettings::get_singleton()->get_setting("physics/2d/default_gravity");
 }
 
-Player::~Player() {
+Player::~Player()
+{
 }
 
-void Player::_physics_process(double delta) {
+void Player::_process(double delta)
+{
+    const Input *input = Input::get_singleton();
+    AnimatedSprite2D *anim = get_node<AnimatedSprite2D>(NodePath("CollisionShape2D/MainBody"));
+
+    if(anim == nullptr)
+        return;
+
+    if(input->is_action_pressed("ui_left"))
+    {
+        lookLeft = true;
+        anim->play("walk_left");
+    }
+    else if(input->is_action_pressed("ui_right"))
+    {
+        lookLeft = false;
+        anim->play("walk_right");
+    }
+    else
+    {
+       anim->play(lookLeft ? "idle_left" : "idle_right");
+    }
+}
+
+void Player::_physics_process(double delta)
+{
 
 	Vector2& velo = get_velocity();
     const Input *input = Input::get_singleton();
--- lib/player.h
+++ lib/player.h
@@ -14,14 +14,16 @@ private:
     const float JUMP_VELOCITY = -400.0f;
 
     float gravity;
+    bool lookLeft;
 
 protected:
     static void _bind_methods();
 
 public:
     Player();
-    ~Player();
+    virtual ~Player();
 
+    void _process(double delta);
     void _physics_process(double delta);
 };
 
